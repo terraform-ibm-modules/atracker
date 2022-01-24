b773b4e3e9b418bdd2cbbebb15f782bff19f5831
Kimberly Hunter <kimberlyhunter@Kimberlys-MBP.lan>
2022-01-24 10:08:21 -0500

    Update variables and ouput.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,22 @@ variable "resource_group" {
   description = "Name of resource group"
 }
 
+variable "target_crn" {
+  type        = string
+  description = "The crn of the Activity Tracking instance"
+}
+
+variable "api_key" {
+  type        = string
+  description = "The IBM Cloud api token"
+}
+
+variable "prefix" {
+  type        = string
+  description = "The prefix name for the service. If not provided it will default to the resource group name"
+  default     = ""
+}
+
 # Atracker target
 variable "atracker_target_name" {
   description = "Name of the atracker target.  Default will be atracker-target-<region>"
@@ -27,4 +43,15 @@ variable "receive_global_events" {
   description = "Indicates whether or not all global events should be forwarded to this region."
   type        = bool
   default     = true
+}
+
+variable "location" {
+  description = "Geographic location of the route (e.g. us-south, us-east"
+  type        = string
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Name of the bucket"
+  default     = ""
 }
